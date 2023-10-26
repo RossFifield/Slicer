daaba8c91dee5be15acc5d7a27615b831a597d9b
RossFifield <33230033+RossFifield@users.noreply.github.com>
2023-10-26 14:55:34 +0100

    Added SwordSwing Animation

---
+++ Assets/Scripts/SwordSwing.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordSwing : MonoBehaviour
+{
+    public GameObject Sword;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartCoroutine(SwordSwing2());
+        }
+       
+    }
+    IEnumerator SwordSwing2()
+    {
+        Sword.GetComponent<Animator>().Play("SwordSwing");
+        yield return new WaitForSeconds(1.0f);
+        Sword.GetComponent<Animator>().Play("NewState");
+    }
+}
